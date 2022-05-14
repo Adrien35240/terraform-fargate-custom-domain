1650a582a3cc6ff93cd40c4779971a15b7cebb33
Adrien35240 <ribaultadrien@orange.fr>
2022-05-14 10:56:22 +0200

    test: update port expose

--- deploy/network.tf
+++ deploy/network.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "security_group_example_app" {
 
     ingress {
         from_port = 80
-        to_port = 3000
+        to_port = 80
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
