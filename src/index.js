7ca36dc92a3dae6817e977b6fb84dc6ba0d1c9a7
Adrien35240 <ribaultadrien@orange.fr>
2022-05-14 11:51:44 +0200

    ci: test new update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 80
 app.set('view engine', 'pug');
 app.get('/', (req, res) => {
-    res.send('Hello World! updated v1')
+    res.send('Hello World! updated v2')
 })
 
 app.listen(port, () => {
